30adc24b12a6a2f6851b58a52100148ea143e6e6
Kenedy de Oliveira Moreira <114973306+Ned1101@users.noreply.github.com>
2023-04-03 08:18:38 -0300

    Add files via upload

---
+++ media de duas notas.py
@@ -0,0 +1,9 @@
+#crie um programa que receba duas notas de um aluno e informe a media da duas notas
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+soma_das_notas = nota1 + nota2
+divisao_das_notas = soma_das_notas/2
+
+print('A media das notas é: ', divisao_das_notas)
